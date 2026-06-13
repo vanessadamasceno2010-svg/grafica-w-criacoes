0e8722eee8c036a37a279d429b59d93318259e54
novo impostor <vanessadamasceno2010@gmail.com>
2026-06-12 22:10:06 -0300

    Delete frontend/src directory

--- frontend/src/main.tsx
+++
@@ -1,2 +0,0 @@
-import React from 'react';import{createRoot}from'react-dom/client';import App from './App';import './index.css';import { AppProvider } from './contexts/AppContext';
-createRoot(document.getElementById('root')!).render(<React.StrictMode><AppProvider><App/></AppProvider></React.StrictMode>);
